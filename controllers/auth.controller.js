8829a340cc50ebeb1b6410803fda03fcafb71c23
kirran01 <Kirran-@live.com>
2023-01-04 17:56:05 -0400

    hashed passwords

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,19 +1,28 @@
 const User = require("../models/User.model");
+const bcryptjs = require("bcryptjs");
 
 const signupController = (req, res) => {
-  //   res.send("post route hit XD");
   const { email, password, username } = req.body;
-  User.create({
-    email,
-    password,
-    username,
-  })
-    .then((createdUser) => {
-      res.send(createdUser);
-    })
-    .catch((err) => {
-      res.send(err);
+  if (!req.body.email || !req.body.password || !req.body.username) {
+    return res.status(400).json({
+      error: {
+        message: "field(s) are missing",
+      },
     });
+  }
+  bcryptjs.hash(password, 10).then((hashedPassword) => {
+    return User.create({
+      email,
+      password: hashedPassword,
+      username,
+    })
+      .then((createdUser) => {
+        res.send(createdUser);
+      })
+      .catch((err) => {
+        res.send(err);
+      });
+  });
 };
 
 module.exports = { signupController };
